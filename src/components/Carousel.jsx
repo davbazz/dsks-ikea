941b9ef3ada2b81f7a68e52a6cf415f6ff6c67fa
David <callmelmao@gmail.com>
2022-10-05 16:05:27 +0100

    first commit

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Carousel.css"
+
+function Carousel() {
+    return 
+}
+
+export default Carousel
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../css/Footer.css"
+
+function Foooter() {
+    return 
+}
+
+export default Foooter
