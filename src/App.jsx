72dbd54e2e8dfff55e4e9181beb60e8b6e337e66
‘Katrin <katrin.baier77@gmail.com>
2022-10-07 09:47:28 +0200

    set up products contentful

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Carousel from './components/Carousel'
-import Cards from './components/Cards'
-import Footer from './components/Footer'
+import React, { useState, useEffect } from 'react';
+import useContentful from './useContentful';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Carousel from './components/Carousel';
+import Cards from './components/Cards';
+import Footer from './components/Footer';
 import './App.css'
 
 function App() {
+	const [products, setProducts] = useState([]);
+	const { getProducts } = useContentful();
 
-  return (
-	<div className="App">
-		<Header />
-		<Hero />
-		<Carousel />
-		<Cards />
-		<Footer />
-	</div>
-  )
+	useEffect(() => {
+		getProducts().then((response) => setProducts(response));
+	}, [])
+
+	return (
+		<div className="App">
+			<h1>Welcome to DSKS</h1>
+			{/* <Header /> */}
+			{/* <Hero /> */}
+			<div className="productCarousel">
+				{products.map((product, index) => <Carousel key={index} product={product} />)}
+			</div>
+			{/* <Cards /> */}
+			{/* <Footer /> */}
+		</div>
+	)
 }
 
 export default App
