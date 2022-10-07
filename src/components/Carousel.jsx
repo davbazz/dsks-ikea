cf9f1dedf60102e710d69755a5614059fa345f91
David <callmelmao@gmail.com>
2022-10-07 11:52:59 +0100

    changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import "../css/Carousel.css"
+import "../css/Carousel.css";
 
-function Carousel() {
-    return 
+const Carousel = ({ product }) => {
+    let productImage;
+    if (product.image !== undefined) {
+        productImage = product.image.fields.file.url
+    } else {
+        productImage = "Sorry, no picture yet"
+    }
+    // console.log(product.productPicture)
+
+    return (
+        <div>
+            <div className="productCard">
+                <img src={productImage} />
+                <h2><a className="link-yellow" href="#">{product.title}</a></h2>
+                <p>{product.description}</p>
+            </div>
+        </div>
+    )
 }
 
 export default Carousel
