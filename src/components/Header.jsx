23bce593b19d77b920ca5d2e188fe034197e5286
Sivak Hico <strukggg@gmail.com>
2022-10-08 22:32:49 +0200

    last changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,48 @@ import React from "react";
 import "../css/Header.css"
 
 function Header() {
-    return 
+    return (
+        <>
+            <div className="nav">
+                <input type="checkbox" id="nav-check" />
+                <div className="nav-header">
+                    <div className="nav-title">
+                        <img className="logo" src="src/img/logo.png" alt="the website logo" />
+                    </div>
+                </div>
+                <div className="nav-btn">
+                    <label for="nav-check">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </label>
+                </div>
+                <div className="nav-links">
+                    <a className='dropdown'>
+                        <span>Products</span>
+                        <div class="dropdown-content">
+                            <ul>
+                                <li>Furniture</li>
+                                <li>Textiles</li>
+                                <li>Decoration</li>
+                                <li>Lighting</li>
+                                <li>Kitchen & appliances</li>
+                                <li>Smart home</li>
+                                <li>Beds & mattresses</li>
+                            </ul>
+                        </div>
+                    </a>
+                    <a>Offers</a>
+                    <a>Best Seller</a>
+                    <a href="#"><input id='search' type="text" placeholder="Search" /></a>
+                    <a href="#"><input className='login' type="button" value='LogIn' /></a>
+                    <a href="#"><input className='signup' type="button" value='SignUp' /></a>
+                    <a href="#"><img className="shopping-cart" src="src/img/Shopping-Cart-icon.png" alt="" /></a>
+                </div>
+            </div>
+            <hr />
+        </>
+    )
 }
 
 export default Header
