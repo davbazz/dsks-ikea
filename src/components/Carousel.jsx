d39a1f1b59d8c96efa157af0a786ba56bb5be66c
Sivak Hico <strukggg@gmail.com>
2022-10-11 00:30:46 +0200

    new category list

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,21 +3,20 @@ import "../css/Carousel.css";
 
 const Carousel = ({ product }) => {
     let productImage;
-    if (product.path !== undefined) {
-        productImage = product.path
+    if (product.image !== undefined) {
+        productImage = product.image
     } else {
         productImage = "Sorry, no picture yet"
     }
-    // console.log(product.productPicture)
 
     return (
-        <div>
+        <>
             <div className="productCard">
                 <img src={productImage} />
-                <h2><a className="link-yellow" href="#">{product.order}</a></h2>
-                <p>{product.category}</p>
+                <h2><a className="link-yellow" href="#">{product.category}</a></h2>
+                <p>{product.title}</p>
             </div>
-        </div>
+        </>
     )
 }
 
